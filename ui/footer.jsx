f5b23f77a236a11d48dca66f8d4adeb724bffd97
nwaxon2020 <princenwachukwu308@yahoo.com>
2025-08-21 18:37:01 +0100

    Home done 4 21/08/25

--- ui/footer.jsx
+++ ui/footer.jsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Footer(){
     return(
-        <div className="flex justify-around text-white py-10 sm:py-14 border-t-1 border-white">
+        <div className="flex justify-around text-white py-14 border-t-1 border-white">
             <ul className="flex flex-col justify-around gap-5">
                 <li className="hover:text-gray-400 transition duration-200 ease-in-out"><Link href={"/"}>Home</Link></li>
                 <li className="hover:text-gray-400 transition duration-200 ease-in-out"><Link href={"/product"}>Products</Link></li>
@@ -14,9 +14,9 @@ export default function Footer(){
                 <li className="hover:text-gray-400 transition duration-200 ease-in-out"><Link href={"/policy/faq"}>FAQ</Link></li>
                 <li>
                     <ul className="flex flex-row justify-around items-center gap-5 sm:gap-10">
-                        <li><Link href={"#add facebook Link"} target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook-square hover:text-gray-400 transition duration-200 ease-in-out" style={{fontSize:"20px", color:"white"}}></i></Link></li>
-                        <li><Link href={"#add twitter Link"} target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter hover:text-gray-400 transition duration-200 ease-in-out" style={{fontSize:"20px"}}></i></Link></li>
-                        <li><Link href={"#add instagram Link"} target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin-square hover:text-gray-400 transition duration-200 ease-in-out" style={{fontSize:"20px"}}></i></Link></li>
+                        <li><Link href={"#add facebook Link"} target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook-square hover:text-gray-400 transition duration-200 ease-in-out" style={{fontSize:"24px", color:"white"}}></i></Link></li>
+                        <li><Link href={"#add twitter Link"} target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter hover:text-gray-400 transition duration-200 ease-in-out" style={{fontSize:"24px"}}></i></Link></li>
+                        <li><Link href={"#add instagram Link"} target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin-square hover:text-gray-400 transition duration-200 ease-in-out" style={{fontSize:"24px"}}></i></Link></li>
                     </ul>
                 </li>
             </ul>
