d950e3e01fc773ae409ee8a04f0b7bc5c2a054ba
nwaxon2020 <princenwachukwu308@yahoo.com>
2025-08-21 15:40:55 +0100

    product done 2

--- ui/product.jsx
+++ ui/product.jsx
@@ -8,13 +8,12 @@ export default function ProductUi(){
     const[menu, setMenu] = useState(false);
 
     return(
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:px-6 lg:px-12 text-gray-200">
-            <section>
-                <div className="hidden sm:block ">Here</div>
-                <div className="relative sm:hidden w-70">
-                    <div onClick={()=> setMenu((prev)=>!prev)} className="absolute top-0 z-20 inline ml-4 p-2 rounded-lg bg-gray-700 font-bold">Catigories <i className={`ml-4 fa ${menu?"fa-angle-up":"fa fa-angle-down"}`}></i></div>
-                    {menu && <ul id="cat" className="max-h-[20rem] z-10 overflow-y-auto w-40 p-4 rounded-b-lg absolute top-[1.4rem] ml-4 bg-gray-500 italics flex flex-col gap-4 items-start justify-center">
-                            <li className="w-full pt-20 p-1 font-semi-bold border-b-1 border-gray-700">Tecno</li>
+        <div className="flex flex-col sm:flex-row sm:gap-4 gap-6 sm:px-6 lg:px-12 text-gray-200">
+            <div className="sm:flex-1 sm:max-w-50">
+                <div className="hidden sm:block ">
+                    <h3 className="pl-1 mt-0 text-lg font-bold border-b-1 border-gray-500 text-gray-500">categories</h3>
+                    <ul className="w-full p-4 pb-50 bg-gray-800 italics flex flex-col gap-4 items-start justify-center rounded-b-lg">
+                            <li className="w-full p-1 font-semi-bold border-b-1 border-gray-700">Tecno</li>
                             <li className="w-full p-1 font-semi-bold border-b-1 border-gray-700">Infinix</li>
                             <li className="w-full p-1 font-semi-bold border-b-1 border-gray-700">Itel</li>
                             <li className="w-full p-1 font-semi-bold border-b-1 border-gray-700">Redmi</li>
@@ -22,21 +21,26 @@ export default function ProductUi(){
                             <li className="w-full p-1 font-semi-bold border-b-1 border-gray-700">Vivo</li>
                             <li className="w-full p-1 font-semi-bold border-b-1 border-gray-700">Others</li>
                             <li className="w-full p-1 ">Assesories</li>
+                    </ul>
+                </div>
+
+                {/* Mobile view Brand Categories */}
+                <div className="relative sm:hidden w-70">
+                    <div onClick={()=> setMenu((prev)=>!prev)} className="absolute top-0 z-20 inline ml-4 p-2 rounded-lg bg-gray-700 font-bold">Categories <i className={`ml-4 fa ${menu?"fa-angle-up":"fa fa-angle-down"}`}></i></div>
+                    {menu && <ul id="cat" className="max-h-[20rem] z-10 overflow-y-auto w-40 p-4 rounded-b-lg absolute top-[1.4rem] ml-4 bg-gray-500 italics flex flex-col gap-4 items-start justify-center">
+                            <li onClick={()=> setMenu(false)} className="w-full pt-30 p-1 font-semi-bold border-b-1 border-gray-700">Tecno</li>
+                            <li onClick={()=> setMenu(false)} className="w-full p-1 font-semi-bold border-b-1 border-gray-700">Infinix</li>
+                            <li onClick={()=> setMenu(false)} className="w-full p-1 font-semi-bold border-b-1 border-gray-700">Itel</li>
+                            <li onClick={()=> setMenu(false)} className="w-full p-1 font-semi-bold border-b-1 border-gray-700">Redmi</li>
+                            <li onClick={()=> setMenu(false)} className="w-full p-1 font-semi-bold border-b-1 border-gray-700">Oppo</li>
+                            <li onClick={()=> setMenu(false)} className="w-full p-1 font-semi-bold border-b-1 border-gray-700">Vivo</li>
+                            <li onClick={()=> setMenu(false)} className="w-full p-1 font-semi-bold border-b-1 border-gray-700">Others</li>
+                            <li onClick={()=> setMenu(false)} className="w-full p-1 ">Assesories</li>
                         </ul>
                     }
                 </div>
-            </section>
-            <section className="mt-12 sm:col-span-4 grid grid-cols-1 sm:grid-cols-3 gap-6 ">
-                <div className="p-4 bg-gray-800 border-1 border-gray-600 rounded-lg">
-                    <Image
-                    
-                        src="/bg.jpeg"
-                        alt=""
-                        width={300}
-                        height={400}
-                        className="mx-auto rounded-lg"
-                    />
-                </div>
+            </div>
+            <div className="sm:mt-6 sm:flex-5 mt-12 sm:mt-0 col-span-1 sm:col-span-4 grid grid-cols-1 sm:grid-cols-3 gap-6 ">
                 <div className="p-4 bg-gray-800 border-1 border-gray-600 rounded-lg">
                     <Image
                     
@@ -207,7 +211,17 @@ export default function ProductUi(){
                         className="mx-auto rounded-lg"
                     />
                 </div>
-            </section>
+                <div className="p-4 bg-gray-800 border-1 border-gray-600 rounded-lg">
+                    <Image
+                    
+                        src="/bg.jpeg"
+                        alt=""
+                        width={400}
+                        height={400}
+                        className="mx-auto rounded-lg"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
